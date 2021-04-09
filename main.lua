536d4c9f2b0e7a7a07635148f87417092c7a6ce2
HeCorr <hccaseclone@gmail.com>
2021-04-09 17:47:39 -0300

    optimization for empty servers (not that it matters much)

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 G_Plugin = nil
+G_Server = nil
 
 function Initialize(Plugin)
 	Plugin:SetName("AlwyasDay")
 	Plugin:SetVersion(1)
 
 	G_Plugin = Plugin
+	G_Server = cRoot:Get():GetServer()
 
 	cPluginManager:AddHook(cPluginManager.HOOK_WORLD_TICK, OnWorldTick);
 
@@ -16,8 +18,10 @@ function OnDisable()
 	LOG(G_Plugin:GetName() .. " is shutting down...")
 end
 
-function OnWorldTick(World, TimeDelta)
-    if World:GetDimension() == dimOverworld then
-        World:SetTimeOfDay(6000)
-    end
+function OnWorldTick(World)
+	if G_Server:GetNumPlayers() > 0 then
+		if World:GetDimension() == dimOverworld then
+			World:SetTimeOfDay(6000)
+		end
+	end
 end
